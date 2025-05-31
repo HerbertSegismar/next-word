8eefea6e51979e94967abb0e82614b074f4f4bb0
Herb Segismar <herbert.segismar@yahoo.com>
2025-05-31 20:00:30 +1200

    About page

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
       >
         <div className="flex flex-col min-h-screen">
           <Navbar />
-          {children}
+          <div className="text-center relative top-20">{children}</div>
           <Footer />
         </div>
       </body>
