2ee7da1e00b70e489260d9f66fdc0a95439aa7f0
Herb Segismar <herbert.segismar@yahoo.com>
2025-06-02 02:31:59 +1200

    2 Timothy 3:16 verse added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { div, p } from "motion/react-client"
 
 export default function Home() {
   return (
-    <>Home Page</>
+    <div className="flex flex-col items-center justify-center mx-10">
+      <p className="text-sm md:text-lg font-bold mt-10">
+        "All scripture is given by inspiration of God, and is
+        profitable for doctrine, for reproof, for correction, for instruction in
+        righteousness."
+      </p>
+      <p className="text-slate-500 text-sm md:text-[16px] top-2 relative">2 Timothy 3:16</p>
+    </div>
   );
 }
--- components/animatedCircles.tsx
+++ components/animatedCircles.tsx
@@ -8,17 +8,17 @@ const animatedCircles = () => {
       <motion.div
         animate={{ scale: [1, 0.8, 1] }}
         transition={{ duration: 2, repeat: Infinity, repeatType: "reverse" }}
-        className="bg-red-500/80 size-10 rounded-full shadow-lg"
+        className="bg-red-500/80 size-10 md:size-12 rounded-full shadow-lg"
       />
       <motion.div
         animate={{ opacity: [0, 1, 0], y: [0, -10, 0] }}
         transition={{ duration: 2, repeat: Infinity, repeatType: "reverse" }}
-        className="bg-green-500/80 size-10 rounded-full shadow-lg"
+        className="bg-green-500/80 size-10 md:size-12 rounded-full shadow-lg"
       />
       <motion.div
         animate={{ scale: [1, 0.8, 1] }}
         transition={{ duration: 2, repeat: Infinity, repeatType: "reverse" }}
-        className="bg-blue-500/80 size-10 rounded-full shadow-lg"
+        className="bg-blue-500/80 size-10 md:size-12 rounded-full shadow-lg"
       />
     </>
   );
