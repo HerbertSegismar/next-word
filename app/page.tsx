5c0ecefaaf560ef680ff789d399e682214077217
Herb Segismar <herbert.segismar@yahoo.com>
2025-06-06 00:00:53 +1200

    Qout

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center mx-10">
       <p className="text-sm md:text-lg font-bold mt-10">
-        "All scripture is given by inspiration of God, and is
+        &quot;All scripture is given by inspiration of God, and is
         profitable for doctrine, for reproof, for correction, for instruction in
-        righteousness."
+        righteousness.&quot;
       </p>
       <p className="text-slate-500 text-sm md:text-[16px] top-2 relative">2 Timothy 3:16</p>
     </div>
